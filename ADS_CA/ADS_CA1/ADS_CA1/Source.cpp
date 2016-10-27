0944a8a870f131467c831bb0509d9a587bb6cd60
Ruairí <ruairimichaelkeogh@gmail.com>
2016-10-27 13:22:27 +0100

    Added starting source for CA

---
+++ ADS_CA/ADS_CA1/ADS_CA1/Source.cpp
@@ -0,0 +1,23 @@
+/* Algorithms and Data Structures 
+				CA 1
+		Plagarism Calculator
+	By Ruairí Keogh - X00121581
+	Graham Lalor - X00122026 */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+
+int main() {
+	string directory = " ",line = " ";
+	int count = 0;
+
+	cout << "Please enter the directory in which your files are stored..." << endl;
+	cin >> directory;
+	
+
+	system("pause");
+	return 0;
+}
